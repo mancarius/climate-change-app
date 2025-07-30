207ed4a332d5c8e5e237165a1b8d9f5f910599ec
Mattia Mancarella <mattia.mancarella.dev@gmail.com>
2025-07-30 15:04:07 +0000

    fix(app.config): Corretto l'URL dell'API globale di riscaldamento

--- apps/temperature/src/app/app.config.ts
+++ apps/temperature/src/app/app.config.ts
@@ -5,13 +5,28 @@ import {
 } from '@angular/core';
 import {
   provideHttpClient,
-  withFetch
+  withFetch,
+  withInterceptors
 } from '@angular/common/http';
+import { globalWarmingApiInterceptor } from '@core/interceptors';
+import { provideGlobalWarmingApi, withApiStrategies, withApiUrl } from '@core/config/global-warming-api.provider';
+import { TemperatureApiStrategy } from '@core/config/api-strategies';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideHttpClient(withFetch())
+    provideHttpClient(
+      withFetch(),
+      withInterceptors([
+        globalWarmingApiInterceptor
+      ])
+    ),
+    provideGlobalWarmingApi(
+      withApiUrl('https://global-warming.org/api'),
+      withApiStrategies([
+        new TemperatureApiStrategy()
+      ])
+    )
   ],
 };
